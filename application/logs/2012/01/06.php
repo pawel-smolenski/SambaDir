b700bc51e57bdf7bffc7c403d5ee9a9141c41c54
Paweł Smoleński <P.Smolenski@stud.elka.pw.edu.pl>
2012-01-07 21:00:21 +0100

    Usunięte logi i założony gitignore na logi i cache

--- application/logs/2012/01/06.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2012-01-06 16:36:48 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
-2012-01-06 16:36:48 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 1126 ]
---
-#0 /home/psmolenski/workspace/SambaDir/index.php(109): Kohana_Request->execute()
-#1 {main}
